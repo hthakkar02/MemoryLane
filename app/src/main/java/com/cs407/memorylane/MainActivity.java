ac648a016f298b2de5e2dec770732ae50034d396
Harsh Thakkar <thakkarharshemail@gmail.com>
2023-11-29 17:16:40 -0600

    Got the image paths for a particular user

--- app/src/main/java/com/cs407/memorylane/MainActivity.java
+++ app/src/main/java/com/cs407/memorylane/MainActivity.java
@@ -28,11 +28,37 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Tested with loadImageReferenceFromUser("/User Data/user000001");
+     * @param owner is the unique identifier for the user in the All Users collection
+     */
+    protected void loadImageReferenceFromUser(String owner){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference ownerRef = db.document(owner); // Create a reference to the owner document
+
+        Log.d("STATUS", "Getting here");
+
+
+        db.collection("All Photos")
+                .whereEqualTo("Owner", ownerRef)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String path = document.getString("Path");
+                            // Retrieve the path to the image in Firebase Storage and display it in your app
+                            Log.d("PhotoData", "This is the path: "+ path);
+                        }
+                    } else {
+                        Log.d("ERRORING", "Error getting documents: ", task.getException());
+                    }
+                });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        loadImageReferenceFromUser("/User Data/user000001");
     }
 }
