108c330b6b14de603d8a8d8fea02c13583efc6d1
Harsh Thakkar <thakkarharshemail@gmail.com>
2023-12-02 18:15:13 -0600

    Added google authentication and manual authentication logic

--- app/src/main/java/com/cs407/memorylane/MainActivity.java
+++ app/src/main/java/com/cs407/memorylane/MainActivity.java
@@ -1,48 +1,146 @@
 package com.cs407.memorylane;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class MainActivity extends AppCompatActivity {
+    protected FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN = 9001;
 
-    /**
-     * Tested with loadImageReferenceFromUser("/User Data/user000001");
-     *
-     * @param owner is the unique identifier for the user in the All Users collection
-     */
-    protected void loadImageReferenceFromUser(String owner) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference ownerRef = db.document(owner); // Create a reference to the owner document
+    protected void googleSignIn() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
-        Log.d("STATUS", "Getting here");
+        GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(this, gso);
 
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
-        db.collection("All Photos")
-                .whereEqualTo("Owner", ownerRef)
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            String path = document.getString("Path");
-                            // Retrieve the path to the image in Firebase Storage and display it in your app
-                            Log.d("PhotoData", "This is the path: " + path);
+    // Add this method to handle the result of the Google Sign-In
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Log.w("Google Sign In Failed", "Google sign in failed", e);
+                // Handle Google Sign-In failure here
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        mAuth = FirebaseAuth.getInstance();
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("Google Sign In Success", "signInWithCredential:success");
+                            // Handle Google Sign-In success here
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // Update UI accordingly
+                        } else {
+                            Log.w("Google Sign In Failed", "signInWithCredential:failure", task.getException());
+                            // Handle Google Sign-In failure here
                         }
-                    } else {
-                        Log.d("ERRORING", "Error getting documents: ", task.getException());
                     }
                 });
+    }
+
+    protected void signUpUser(String email, String password){
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Successful User Sign up", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Failed User Sign up", "createUserWithEmail:failure", task.getException());
+
+                        }
+                    }
+                });
+    }
+    protected void signInUser(String email, String password) {
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Successful User Sign In", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Failed User Sign In", "signInWithEmail:failure", task.getException());
+                        }
+                    }
+                });
+    }
+
+    protected void accessUserInfo() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+        }
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadImageReferenceFromUser("/User Data/user000001");
+
+
+        //signUpUser("mehul@gmail.com", "hehehaha");
+//        signInUser("mehul@gmail.com", "hehehaha");
+        googleSignIn();
+        //TODO add method to extract user data
     }
 }
