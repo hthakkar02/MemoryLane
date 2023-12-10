37a19cb887ee09316541c64953299b36443e5408
manoj <msbonam@wisc.edu>
2023-12-10 04:58:21 -0600

    Immplemented User Profile Page to show Username and Mem Made

--- app/src/main/java/com/cs407/memorylane/UserProfileFragment.java
+++ app/src/main/java/com/cs407/memorylane/UserProfileFragment.java
@@ -7,10 +7,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -36,25 +44,52 @@ public class UserProfileFragment extends Fragment {
 
         Log.d("User ID is:", userID);
 
+        // Access TextView here after the view has been created
+        TextView userName = view.findViewById(R.id.tvUserName);
+        TextView memoryCount = view.findViewById(R.id.tvMemoriesMade);
+
+        // Now you can use UserName to manipulate the TextView
+        userName.setText("User: " + userID);
 
         setProfileInfo();
     }
 
 
     public void setProfileInfo() {
-        // Retrieve information about dude from userID
-        dataTest dataTest = new dataTest();
-        dataTest.retrieveUserInfo(userID, info -> {
-            for (String element : info) {
-                Log.d("Profile Info Test", element);
+        dataTest dT = new dataTest();
+        dT.retrieveUserInfo(userID, new dataTest.UserInfoCallback() {
+            @Override
+            public void onUserInfoRetrieved(ArrayList<String> userInfo) {
+                if (userInfo != null && !userInfo.isEmpty()) {
+                    // Assuming the userInfo array contains the username at index 0 and memories made at index 1
+                    String username = userInfo.get(0);
+                    String memoriesMade = userInfo.get(1);
+
+                    // Update UI here with the fetched data
+                    TextView userName = getView().findViewById(R.id.tvUserName);
+                    TextView memoryCount = getView().findViewById(R.id.tvMemoriesMade);
+
+                    userName.setText(username);
+                    memoryCount.setText("Memories Made: " + memoriesMade);
+                }
             }
         });
     }
-
-
-
-
-
-
-
 }
+
+
+
+
+
+            //dataTest dataTest = new dataTest();
+//        dataTest.retrieveUserInfo(userID, info -> {
+//            for (String element : info) {
+//                Log.d("Profile Info Test", element);
+//            }
+//        });
+
+
+        //username
+        //memories created 0814
+
+
